4696d4fbdf7277aca547f925da1af8b1f4ca27eb
SlytherinKid <mr.shubhamkr2000@gmail.com>
2025-04-07 22:23:51 +0530

    added cards animation and dynamic rendering

--- docs-app/src/components/Card.jsx
+++ docs-app/src/components/Card.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import { SiGoogledocs } from "react-icons/si";
 import { FaDownload } from "react-icons/fa6";
+import { RiEyeCloseLine } from "react-icons/ri";
+import { motion } from "framer-motion";
 
-const Card = () => {
+const Card = ({data, reference}) => {
   return (
-        <div className='relative w-60 h-72 rounded-[40px] bg-zinc-900/50 text-zinc-200 px-8 py-10 overflow-hidden'>
+        <motion.div drag dragConstraints={reference} whileDrag={{scale:1.2}} dragElastic={0.2} dragTransition={ {bouncStiffness: 100, bounceDamping: 10}}
+         className='relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-900/50 text-zinc-200 px-8 py-10 overflow-hidden'>
             <SiGoogledocs />
-            <p className='text-sm comfortaa tracking-tight leading-tight mt-5 font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet architecto alias quod?</p>
+            <p className='text-sm comfortaa tracking-tight leading-tight mt-5 font-semibold'>{data.desc}</p>
             <div className="card-footer absolute bottom-0 w-full left-0">
               <div className='flex items-center justify-between py-3 px-8 mb-2 text-zinc-400'>
-                <h5 className='hover:text-zinc-200'>.4mb</h5>
-                <FaDownload size=".8em" className='hover:text-zinc-200'/>
+                <h5 className='hover:text-zinc-200'>{data.fileSize}</h5>
+                {data.close ? <RiEyeCloseLine size=".9em" className='hover:text-zinc-200'/> : <FaDownload size=".8em" className='hover:text-zinc-200'/>}
               </div>
-              <div className="tag w-full py-4 flex items-center justify-center bg-green-400">
-                <h3 className="text-sm font-semibold comfortaa">Download</h3>
-              </div>
+              { data.tag.isOpen && (<div className={`tag w-full py-4 flex items-center justify-center ${data.tag.tagColor === "blue" ? "bg-blue-500" : "bg-green-500"}`}>
+                <h3 className="text-sm font-semibold comfortaa">{data.tag.tagTitle}</h3>
+              </div>)}
+              
             </div>
-        </div>
+        </motion.div>
   )
 }
 
--- docs-app/src/components/Foreground.jsx
+++ docs-app/src/components/Foreground.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Card from './Card'
 
 
 const Foreground = () => {
+
+    const ref = useRef(null);
+    const data = [{
+      desc:"Lorem ipsum, Hello hello dolor sit amet consectetur adipisicing elit. Dolor earum nesciunt facere?",
+      fileSize: ".9mb",
+      close: true,
+      tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green"},
+    },
+    { 
+      desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, maxime!",
+      fileSize: ".7mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Upload", tagColor: "blue"},
+    },{ 
+      desc:"Lorem ipsum dolor sit amet, consectetur adipisicing.",
+      fileSize: ".3mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green"},
+    },{
+      desc:"Lorem ipsum, Hello hello dolor sit amet consectetur adipisicing daddyy yeahh elit. Dolor earum nesciunt facere?",
+      fileSize: "1.1mb",
+      close: true,
+      tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green"},
+    },
+  ];
   return (
     <div>
-      <div className='fixed z-[3] w-full h-full top-0 left-0'>
-        <Card />
+      <div ref={ref} className='fixed z-[3] w-full h-full top-0 left-0 flex gap-8 flex-wrap p-10'>
+        {data.map((item, index) =>(
+          <Card data={item} reference={ref} />
+        ))}
       </div>
     </div>
   )
